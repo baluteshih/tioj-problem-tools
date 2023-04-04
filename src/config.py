ac799b33671f0a0ed19b9e1ddd7dc84b3e99c2b3
baluteshih <brian369999@gmail.com>
2023-04-04 16:33:42 +0800

    [Feature] User can now use enviornment variable CONFIGS_DIR to specifiy a custom configs directory in other places

--- src/config.py
+++ src/config.py
@@ -1,8 +1,12 @@
 from dynaconf import Dynaconf
 import os
 
+CONFIGS_DIR = os.environ.get('CONFIGS_DIR')
+if not CONFIGS_DIR:
+    CONFIGS_DIR = 'configs'
+
 settings = Dynaconf(
-    settings_files=[os.path.join(os.path.dirname(os.path.dirname(__file__)), 'configs', 'default_settings.toml'),
-                    os.path.join(os.path.dirname(os.path.dirname(__file__)), 'configs', 'endpoints_settings.toml'),
-                    os.path.join(os.path.dirname(os.path.dirname(__file__)), 'configs', 'path_settings.toml'),
-                    os.path.join(os.path.dirname(os.path.dirname(__file__)), 'configs', 'tioj_instance_settings.toml')])
+    settings_files=[os.path.join(os.path.dirname(os.path.dirname(__file__)), CONFIGS_DIR, 'default_settings.toml'),
+                    os.path.join(os.path.dirname(os.path.dirname(__file__)), CONFIGS_DIR, 'endpoints_settings.toml'),
+                    os.path.join(os.path.dirname(os.path.dirname(__file__)), CONFIGS_DIR, 'path_settings.toml'),
+                    os.path.join(os.path.dirname(os.path.dirname(__file__)), CONFIGS_DIR, 'tioj_instance_settings.toml')])
